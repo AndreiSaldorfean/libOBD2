41145eec23d4bae3e7fe7f76e86b2d250d43245c
AndreiSaldorfean <andreisaldorfean@gmail.com>
2026-02-06 23:01:59 +0200

    feature: Add initial library architecture

--- include/libobd2.h
+++ include/libobd2.h
@@ -1,12 +1,24 @@
-#ifndef KLINE_H
-#define KLINE_H
+#ifndef L3_KWP_H
+#define L3_KWP_H
 
 /* ================================================ INCLUDES =============================================== */
+#include "srv_status.h"
+#include "data_link_if.h"
+#include <stddef.h>
+#include "stdbool.h"
+
 /* ================================================= MACROS ================================================ */
 /* ======================================= TYPEDEFS, ENUMS, STRUCTS ======================================== */
+typedef struct
+{
+    const dataLink_if_t *pDataLink;
+    bool connectionStatus;
+}obd_ctx_t;
+
 /* ============================================ INLINE FUNCTIONS =========================================== */
 /* ======================================= EXTERN GLOBAL VARIABLES ========================================= */
 /* =============================================== MODULE API ============================================== */
-int foo();
+obd_status_t LibOBD2_Init(obd_ctx_t *ctx);
+obd_status_t LibOBD2_RequestService(obd_ctx_t *ctx, uint8_t sid, uint8_t pid, uint32_t *response_buffer, size_t *response_len);
 
-#endif /* KLINE_H */
+#endif /* L3_KWP_H */
