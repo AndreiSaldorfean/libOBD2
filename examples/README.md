a1415bf91e9cc714f0334585e4fce0e172f35f2d
Andrei Saldorfean <79752722+AndreiSaldorfean@users.noreply.github.com>
2026-02-15 00:35:04 +0200

    Revert "24 add base implementation for timing interface (#26)"
    This reverts commit 5741e6659101a0afffe629f8525c3b842ecd30d2.

--- examples/README.md
+++
@@ -1,4 +0,0 @@
-# Structure
-
-libs/ - contains the external libraries that each example will need to compile
-STM32F401CCU - is the actual example that shows a demo application of how to use libOBD2
--- scripts/add_path_to_comp_com.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-path=" -I/home/rudy/tools/arm-gnu-toolchain/arm-none-eabi/include/\","
-
-sed --help
-# sed "4s/..$/${path}/" ../build/compile_commands.json > tmp && mv tmp ../build/compile_commands.json
