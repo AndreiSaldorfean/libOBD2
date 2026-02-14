a018e3692b6bdb937c8848a5316bb193ee008d8f
AndreiSaldorfean <andreisaldorfean@gmail.com>
2026-02-14 22:44:49 +0200

    feature: Add base implementation for timer interface
    Signed-off-by: AndreiSaldorfean <andreisaldorfean@gmail.com>

--- tests/src/app/test_timer.c
+++ tests/src/app/test_timer.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <time.h>
 #include "kwp_timer.h"
+#include "utils.h"
 
 /* ================================================= MACROS ================================================ */
 /* ============================================ LOCAL VARIABLES ============================================ */
@@ -18,6 +19,7 @@
 /* ================================================ MODULE API ============================================= */
 void test_TIMER_0(void)
 {
+    bool timeoutExpired = true;
     timerCtx_t tmrCtx =
     {
         .timeout_active = false,
@@ -27,7 +29,29 @@ void test_TIMER_0(void)
 
     KWP_TMR_Init(&tmrCtx);
 
-    KWP_TMR_DelayMs(&tmrCtx, 10);
+    KWP_TMR_DelayMs(&tmrCtx, 1000);
+
+    KWP_TMR_StartTimeout(&tmrCtx, 1000, NULL, NULL);
+
+    for(int i=0;i<10000;i++)
+    {
+        __asm__("nop");
+    }
+
+    timeoutExpired = KWP_TMR_IsTimeoutExpired(&tmrCtx);
+
+    KWP_TMR_StopTimeout(&tmrCtx);
+
+    KWP_TMR_StartTimeout(&tmrCtx, 10, NULL, NULL);
+
+    for(int i=0;i<1000000;i++)
+    {
+        __asm__("nop");
+    }
+
+    timeoutExpired = KWP_TMR_IsTimeoutExpired(&tmrCtx);
+
+    KWP_TMR_StopTimeout(&tmrCtx);
 }
 
 void test_TIMER_1(void)
@@ -40,6 +64,18 @@ void test_TIMER_1(void)
         .timeout_user_data = NULL,
         .timeout_target_ms = 0,
     };
+    uint32_t time1 = 0;
+    uint32_t time2 = 0;
+    uint32_t elapsed = 0;
 
     KWP_TMR_Init(&tmrCtx);
+
+    time1 = KWP_TMR_GetTimeMs(&tmrCtx);
+
+    KWP_TMR_DelayMs(&tmrCtx, 10);
+
+    time2 = KWP_TMR_GetTimeMs(&tmrCtx);
+
+    elapsed = time2 - time1;
+    TEST_ASSERT_EQUAL(elapsed, 10);
 }
